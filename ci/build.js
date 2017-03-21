279b802f8139d50bbaf9b39b59d6ddac0a839c13
Taka Okunishi <okunishinish@gmail.com>
2017-03-21 12:05:31 +0900

    Add api links on README

--- ci/build.js
+++ ci/build.js
@@ -14,7 +14,8 @@ const coz = require('coz')
 runTasks('build', [
   () => coz.render([
     '.*.bud',
-    'lib/.*.bud',
+    'lib/**/.*.bud',
+    'doc/**/.*.bud',
     'test/.*.bud'
   ])
 ], true)
---
+++ misc/signature.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const jsdoc = require('../jsdoc.json')
+const { snakecase } = require('stringcase')
+
+const paramDesc = ({ parameters = [] }) => parameters
+  .map((param) => param.name)
+  .filter((name) => !/\./.test(name)).join(', ')
+const asClass = (item) => Object.assign({}, item, {
+  functions: (item.functions || []).map((func) => Object.assign(func, {
+    paramDesc: paramDesc(func)
+  })),
+  constructor: Object.assign(item.constructor, {
+    paramDesc: paramDesc(item.constructor)
+  }),
+  instanceName: snakecase(item.name).split('_').pop()
+})
+const asFunc = (item) => Object.assign({}, item, {
+  paramDesc: paramDesc(item)
+})
+const byName = (list, { wrapper = (item) => item }) => list.reduce((result, item) => Object.assign(result, {
+  [item.name]: wrapper(item)
+}), {})
+
+let classes = byName(jsdoc[ 'classes' ], { wrapper: asClass })
+let functions = byName(jsdoc[ 'functions' ], { wrapper: asFunc })
+let { create, isLump } = functions
+
+module.exports = {
+  classes: Object.keys(classes)
+    .filter((name) => /^Clay/.test(name) || /Mixed$/.test(name))
+    .map((name) => classes[ name ])
+    .filter(Boolean),
+  functions: [
+    create,
+    isLump
+  ].filter(Boolean)
+}
+
+if (!module.parent) {
+  console.log(module.exports)
+}
